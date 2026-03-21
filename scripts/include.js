abed854f4e306eda29a6f009ec0353b333209089
Muhammad Ridha Ridwan <reedjaz.reedjaz@gmail.com>
2026-03-22 05:44:22 +0700

    Tablet Responsive 1

--- scripts/include.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('[data-include]').forEach(el =>
-  fetch(el.dataset.include)
-    .then(r => r.ok ? r.text() : null)
-    .then(t => t && (el.innerHTML = t))
-    .catch(()=>{})
-)
